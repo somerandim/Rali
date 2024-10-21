1f26c7cd0b29bb9310150eedd7252c0807f9b145
somerandim <rickandmorty2006@gmail.com>
2024-10-22 00:53:00 +0300

    the new folders

---
+++ Rali-Server/src/main/java/Rali/SportsCenter/SportsCenterApplication.java
@@ -0,0 +1,13 @@
+package Rali.SportsCenter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportsCenterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SportsCenterApplication.class, args);
+	}
+
+}
---
+++ Rali-Server/src/test/java/Rali/SportsCenter/SportsCenterApplicationTests.java
@@ -0,0 +1,13 @@
+package Rali.SportsCenter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SportsCenterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
