8c588f241c03ee4a85c7f122aad2d7ef33a13469
Rona Elias <ronaelias1@gmail.com>
2024-12-02 22:29:05 +0200

    footer added

--- Rali-Client/src/app/app.component.ts
+++ Rali-Client/src/app/app.component.ts
@@ -3,12 +3,13 @@ import { RouterOutlet } from '@angular/router';
 import { AdminComponent } from "./admin/admin.component";
 import { HomeComponent } from "./features/home/home.component";
 import { NavbarComponent } from './features/navbar/navbar.component';
+import { FooterComponent } from './features/footer/footer.component';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, AdminComponent, NavbarComponent],
+  imports: [RouterOutlet, AdminComponent, NavbarComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
